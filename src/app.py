5d5c7f6ce3ff5afd35923e2535c51f4f99b39eb8
Vitalii Vokhmin <vitaliy.vokhmin@gmail.com>
2017-07-13 09:18:39 +0200

    Simplify code

--- src/app.py
+++ src/app.py
@@ -12,28 +12,13 @@ BOT_MENTION_NAME = '@{}'.format(BOT_NAME.lower())
 bot = aiotg.Bot(api_token=BOT_TOKEN, name=BOT_NAME)
 
 
-async def say_hello(chat):
-    msg = 'Hello {} from EuroPython 2017!'.format(chat.sender.get('first_name') or '%username%')
-    return await chat.reply('msg')
-
-
 @bot.command(r'/hello')
 async def hello(chat, match):
-    return await say_hello(chat)
-
-
-@bot.default
-async def default_handler(chat, message):
-    if BOT_MENTION_NAME in message.lower():
-        return await say_hello(chat)
+    msg = 'Hey {}! Welcome to EuroPython 2017!'.format(
+        chat.sender.get('first_name') or '%username%')
+    return await chat.reply(msg)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.loop())
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        bot.stop()
-        loop.stop()
+    loop.run_until_complete(bot.loop())
